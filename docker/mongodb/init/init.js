a82eeedfad08aef93f0f204a0ab3590ebcd92615
JP3Pe <rlaworhkd430430@gmail.com>
2021-11-28 18:33:07 +0900

    up: 기본 DB 생성후 기본값 insertMany()처리

---
+++ docker/mongodb/init/init.js
@@ -0,0 +1,62 @@
+db.rooms.insertMany([
+    {
+        value: 'Airbnb',
+        photo: 'https://s3.eu-west-2.amazonaws.com/book-room-platform/airbnb.jpg'
+    },
+    {
+        value: 'Autodesk',
+        photo: 'https://s3.eu-west-2.amazonaws.com/book-room-platform/autodesk.jpg'
+    },
+    {
+        value: 'Conde Nast Ent.',
+        photo: 'https://s3.eu-west-2.amazonaws.com/book-room-platform/condenastent.jpg'
+    },
+    {
+        value: 'Decom',
+        photo: 'https://s3.eu-west-2.amazonaws.com/book-room-platform/decom.jpg'
+    },
+    {
+        value: 'Fairphone',
+        photo: 'https://s3.eu-west-2.amazonaws.com/book-room-platform/fairphone.jpg'
+    },
+    {
+        value: 'Grupocp',
+        photo: 'https://s3.eu-west-2.amazonaws.com/book-room-platform/grupocp.jpg'
+    },
+    {
+        value: 'Heavybit Ind.',
+        photo: 'https://s3.eu-west-2.amazonaws.com/book-room-platform/heavybitindustries.jpg'
+    },
+    {
+        value: 'IsIs',
+        photo: 'https://s3.eu-west-2.amazonaws.com/book-room-platform/isisoffice.jpg'
+    },
+    {
+        value: 'Leo Burnett',
+        photo: 'https://s3.eu-west-2.amazonaws.com/book-room-platform/leoburnetthq.jpg'
+    },
+    {
+        value: 'Loft',
+        photo: 'https://s3.eu-west-2.amazonaws.com/book-room-platform/loft.jpg'
+    },
+    {
+        value: 'Pinterest',
+        photo: 'https://s3.eu-west-2.amazonaws.com/book-room-platform/pinterest.jpg'
+    },
+    {
+        value: 'Skyscanner',
+        photo: 'https://s3.eu-west-2.amazonaws.com/book-room-platform/skyscanner.jpg'
+    },
+    {
+        value: 'Truly Madly',
+        photo: 'https://s3.eu-west-2.amazonaws.com/book-room-platform/trulymadlyoffice.jpg'
+    },
+    {
+        value: 'WeWork',
+        photo: 'https://s3.eu-west-2.amazonaws.com/book-room-platform/wework.jpg'
+    },
+    {
+        value: 'Zendesk',
+        photo: 'https://s3.eu-west-2.amazonaws.com/book-room-platform/zendesk.jpg'
+    }]
+);
